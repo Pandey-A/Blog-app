0cfa27cc13dcc7b6158c99ea4ea1b7650d9cc8fd
Ashutosh Pandey <ashutoshpandey@Ashutosh-ka-MacBook-Air.local>
2024-10-06 23:39:22 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const articleRouter = require("./routes/articles")
+const Article = require('./models/article')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb+srv://aspandey1904:Pandey1926@cluster0.c5qll.mongodb.net/',{
+    dbName:"Blog_App"
+})
+app.set("views", "./view")
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.get('/', async(req, res) => {
+    const articles =await Article.find().sort({ createdAt:'desc'})
+    res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(4000)
